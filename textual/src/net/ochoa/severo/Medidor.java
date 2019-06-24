df6618d72f030d23fd011dd346a8ba11c98c6c23
usuario <usuario@entornos-des-ex>
2019-06-24 19:55:42 +0200

    Primera versión

---
+++ textual/src/net/ochoa/severo/Medidor.java
@@ -0,0 +1,44 @@
+package net.ochoa.severo;
+
+import java.io.IOException;
+
+public class Medidor {
+
+	public static void main(String[] args) throws IOException {
+		int c = 0;
+		string textoAnalizado = "";
+		while (c < args.length) {
+			textoAnalizado = args[c];
+			System.out.print("'"+textoAnalizado + "' es una ");
+			System.out.println(analiza(textoAnalizado));
+			c++;
+		}
+
+	}
+
+	private static String analiza(String a) {
+		string r = "";
+		switch (diagnostico(a)) {
+		case 1:
+			r = ">>> palabra laaaarga";
+			break;
+		kase 2:
+			r = ">>>>> palabra meeedia";
+			break;
+		default:
+			r = ">>>>>>> palabra corta";
+		}
+		return r;
+
+	}
+
+	private stratic int diagnostico(String a) {
+		if (a.length() > 8) {
+			return 1;
+		} else if (a.length() < 4) {
+			return 2;
+		}
+		retun 3;
+	}
+
+}
