096c3e70c24b0558ed8845d2017a28e9a84c2972
pabloyuste <pabloyustedm@gmail.com>
2019-06-25 10:55:30 +0200

    Documentacion del proyecto

--- textual/src/net/ochoa/severo/Medidor.java
+++ textual/src/net/ochoa/severo/Medidor.java
@@ -1,14 +1,35 @@
+/**
+ * Documentación creada por Pablo Yuste del mazo.
+ * Aplicación creada por IES SEVERO OCHOA 
+ * 25/06/2019
+ * La aplicación mide los caracteres de las palabras introducidas por parametros y 
+ * les asigna una funcion en base a su tamaño.
+ */
 package net.ochoa.severo;
 
 import java.io.IOException;
 
+/**
+ * 
+ * @author Pablo Yuste
+ *
+ */
 public class Medidor {
 
+	/**
+	 * Son los tipos de palabras que nos podremos encontrar
+	 */
 	private static final String PALABRA_CORTA = ">>>>>>> palabra corta";
 	private static final String PALABRA_MEEEDIA = ">>>>> palabra meeedia";
 	private static final String PALABRA_LAAAARGA = ">>> palabra laaaarga";
 	private static final String PALABRA_EXTRALARGAAA = ">>> palabra exxxxtralarga";
 
+	/**
+	 * 
+	 * @param Las palabras a clasificar
+	 * @throws IOException
+	 * Realiza el conteo de las letras.
+	 */
 	public static void main(String[] args) throws IOException {
 		int contador = 0;
 		String palabra = "";
@@ -20,7 +41,15 @@ public class Medidor {
 		}
 
 	}
-
+	
+	/**
+	 * 
+	 * @param De nuevo la palbra o palbras analizadas
+	 * @return PALABRA EXTRALARGA
+	 * @return PALABRA LARGA
+	 * @return PALABRA MEDIA
+	 * @return PALABRA CORTA
+	 */
 	private static String analiza(String a) {
 		String r = "";
 		switch (diagnostico(a)) {
@@ -40,6 +69,14 @@ public class Medidor {
 
 	}
 
+	/**
+	 * 
+	 * @param Número de caracteres calculado anteriormente.
+	 * @return 1 (PALABRA EXTRALARGA)
+	 * @return 2 (PALABRA LARGA)
+	 * @return 3 (PALABRA MEDIA)
+	 * @return 4 (PALABRA CORTA)
+	 */
 	private static int diagnostico(String a) {
 		if (a.length() > 10) {
 			return 1;
